ca18923d62961a50871ded5e559588d70b346544
yang0 <yang0@DESKTOP-RKO6ACO>
2025-06-09 17:50:47 +0900

    JAVA3導入‐自己紹介

---
+++ Hello World_YouChouhan/src/sample/SelfIntroduction.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "楊　超凡";
+		int age = 34;
+		String hobby = "読書";
+
+		System.out.println("私の名前は"+name+"です。");
+		System.out.println("年齢は"+age+"です。");
+		System.out.println("趣味は"+hobby+"です。");
+
+
+	}
+
+}
