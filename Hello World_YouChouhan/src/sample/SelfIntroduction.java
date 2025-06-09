387db91ae640660b72dd107acec3e038d2cbefc0
yang0 <yang0@DESKTOP-RKO6ACO>
2025-06-09 17:59:40 +0900

    JAVA３‐自己紹介（コメント入り）

--- Hello World_YouChouhan/src/sample/SelfIntroduction.java
+++ Hello World_YouChouhan/src/sample/SelfIntroduction.java
@@ -3,10 +3,15 @@ package sample;
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
+		//氏名 
 		String name = "楊　超凡";
+		//年齢
 		int age = 34;
+		//趣味
 		String hobby = "読書";
-
+		/*
+		 * 自己紹介文を出力する処理
+		 */
 		System.out.println("私の名前は"+name+"です。");
 		System.out.println("年齢は"+age+"です。");
 		System.out.println("趣味は"+hobby+"です。");
